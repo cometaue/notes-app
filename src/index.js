18141c3dbbaf01645d00d7c1696b323ea8cb59bf
andres <cometaue@gmail.com>
2020-04-15 15:34:59 -0400

    se modifico el doteenv

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 /* ******************************** */
 const app = require('./server').app;
 const db = require('./database');
-require('colors');
 /* ******************************** */
 /*                server                */
 /* ******************************** */
